dc49254e539559a504c42066a17966ad87c58598
Vaulverin <vauulin@gmail.com>
2014-01-18 20:06:26 +0600

    csv

--- app/models/model_test.php
+++ app/models/model_test.php
@@ -12,7 +12,14 @@ class Model_Test extends Model
                 3 => array(0 => 'array in array in array oO')
             )
         );
-        generateJSONReply($arr);
+        //generateJSONReply($arr);
+        header("Content-type: text/csv");
+        header("Content-Disposition: attachment; filename=file.csv");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        echo '1997,Ford,E350,"ac, abs, moon",3000.00\n
+        1999,Chevy,"Venture ""Extended Edition""","",4900.00\n
+1996,Jeep,Grand Cherokee,"MUST SELL! air, moon roof, loaded",4799.00';
     }
 
 }
