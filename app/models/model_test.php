52d3bcc7e9f23201405fc81bac21496acd99a29a
Vaulverin <vauulin@gmail.com>
2014-01-18 20:30:36 +0600

    preparing

--- app/models/model_test.php
+++ app/models/model_test.php
@@ -12,11 +12,10 @@ class Model_Test extends Model
                 3 => array(0 => 'array in array in array oO')
             )
         );
+        $sig = sha1('4929fb06-33f7-474f-a8f7-f15caf02772a'.$_GET['token']);
         //generateJSONReply($arr);
-        header("Content-type: text/csv");
-        header("Content-Disposition: attachment; filename=file.csv");
-        header("Pragma: no-cache");
-        header("Expires: 0");
+        header("appid : 274");
+        header("sig : ".$sig);
         echo '1997,Ford,E350,"ac, abs, moon",3000.00\n
         1999,Chevy,"Venture ""Extended Edition""","",4900.00\n
 1996,Jeep,Grand Cherokee,"MUST SELL! air, moon roof, loaded",4799.00';
